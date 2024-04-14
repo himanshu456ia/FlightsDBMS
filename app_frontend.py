69c317838a93d8ab4eb0db84d1120ba31ba3f981
Oishik Chowdhury <oishik03c@gmail.com>
2024-04-14 07:16:25 +0530

    improved space management of window object

--- app_frontend.py
+++ app_frontend.py
@@ -12,45 +12,50 @@ class Starting_window:
         self.root.title("Flight Database")
         window_width = self.root.winfo_screenwidth()
         window_height = self.root.winfo_screenheight()
+        self.root.geometry(f"{window_width//4}x{window_height//4}")
+        self.root.resizable(width=False, height=False)
         
         # Frame
         main_frame = tk.Frame(self.root)
-        main_frame.pack(fill='both')
+        main_frame.pack(fill='both', expand=True)
 
-        title_frame = tk.Frame(main_frame)
+        title_frame = tk.Frame(main_frame, bg='gray')
         title_frame.columnconfigure(0, weight=1)
-        title_frame.pack(fill='x')
+        title_frame.rowconfigure(0,weight=1)
+        title_frame.pack(fill='both', expand=True, padx=10, pady=5)
 
         query_frame = tk.Frame(main_frame)
         query_frame.columnconfigure(0, weight=1)
         query_frame.columnconfigure(1, weight=1)
-        query_frame.pack(fill='x')
+        query_frame.rowconfigure(0, weight=1)
+        query_frame.rowconfigure(1, weight=2)
+        query_frame.pack(fill='both', expand=True, padx=40, pady=10)
 
         action_frame = tk.Frame(main_frame)
         # action_frame.columnconfigure(0, weight=5)
         # action_frame.columnconfigure(1, weight=1)
         # action_frame.columnconfigure(2, weight=1)
-        action_frame.pack(side='bottom', fill='both', padx=10, pady=5)
+        action_frame.pack(side='bottom', fill='x', padx=10, pady=5)
 
         # Labels
-        self.lbl_title = tk.Label(title_frame, text="Flight Enquiry System", font='Arial 20 bold', bg='grey')
-        self.lbl_title.grid(row=0, column=0, sticky='nwes', padx=20, pady=5)
+        self.lbl_title = tk.Label(title_frame, text="Flight Enquiry System", font='Arial 20 bold')
+        self.lbl_title.grid(row=0, column=0, sticky='nwes', padx=10, pady=5)
 
         self.query_title = tk.Label(query_frame, text="What would you like to enquire about?", font='Arial 12')
-        self.query_title.grid(row=0, columnspan=2, sticky='wnes', padx=40, pady=5)
+        self.query_title.grid(row=0, columnspan=2, sticky='nsew')
         
         # Radiobutton to choose DB
         self.v = tk.StringVar(query_frame, 'None')
         self.rdiobtn_flight = tk.Radiobutton(query_frame, text="Flights", value='F', variable=self.v)
-        self.rdiobtn_flight.grid(row=1, column=0, sticky='nwes')
+        self.rdiobtn_flight.grid(row=1, column=0, sticky='nsew')
         self.rdiobtn_passengers = tk.Radiobutton(query_frame, text="Passengers", value='P', variable=self.v)
-        self.rdiobtn_passengers.grid(row=1, column=1, sticky='nwes')        
+        self.rdiobtn_passengers.grid(row=1, column=1, sticky='nsew')        
 
         # Buttons
         self.btn_exit = tk.Button(action_frame, text='Quit', font='Arial 12', command=self.on_closing)
-        self.btn_exit.pack(side='right')
+        self.btn_exit.pack(side='right', fill='y')
         self.btn_done = tk.Button(action_frame, text='Next', font='Arial 12', command=self.on_done)
-        self.btn_done.pack(side='right')
+        self.btn_done.pack(side='right', fill='y')
 
         # Protocol
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
