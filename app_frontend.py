cceb29ce1aacd41ad1290c39fdd733b0da3c7bd5
Oishik Chowdhury <oishik03c@gmail.com>
2024-04-15 08:22:46 +0530

    added passenger db capabilities

--- app_frontend.py
+++ app_frontend.py
@@ -20,8 +20,8 @@ class Starting_window:
 
         if not os.path.isfile("Databases/flights.db"):
             fbe.init_db()
-        # if not os.path.isfile("Databases/passengers.db"):
-        #     pbe.init_db()
+        if not os.path.isfile("Databases/passengers.db"):
+            pbe.init_db()
 
         # Frame
         main_frame = tk.Frame(self.root)
@@ -79,14 +79,14 @@ class Starting_window:
             messagebox.showinfo(title="Help", message="Choose one of the options!")
         elif self.v.get()=='F':
             clear_frame(self.root)
-            flight_window = Flight_window(self)
+            flight_window = Flight_window(self.root)
         elif self.v.get()=='P':
             clear_frame(self.root)
-            passenger_window = Passenger_window(self)
+            passenger_window = Passenger_window(self.root)
 
 class Flight_window(Starting_window):
-    def __init__(self, starting_window):
-        self.root = starting_window.root
+    def __init__(self, master):
+        self.root = master
         self.root.title("Flights Enquiry Database")
         self.root.geometry("1260x720")
 
@@ -109,6 +109,7 @@ class Flight_window(Starting_window):
             FlightList.delete(1, tk.END)
             for row in fbe.ViewAllData():
                 FlightList.insert(tk.END, row)
+        
         def searchflightdb():
             FlightList.delete(1, tk.END)
             SearchTerm = {}
@@ -229,10 +230,170 @@ class Flight_window(Starting_window):
 
 
 class Passenger_window(Starting_window):
-    def __init__(self, starting_window):
-        self.root = starting_window.root
+    def __init__(self, master):
+        self.root = master
         self.root.title("Passengers Enquiry Database")
         self.root.geometry("1260x720")
+        
+        
+        PassengerId=tk.StringVar()
+        FName = tk.StringVar()
+        LName = tk.StringVar()
+        Gender=tk.StringVar()
+        Age=tk.StringVar()
+        Nationality=tk.StringVar()
+        CountryCode = tk.StringVar()
+        CountryName=tk.StringVar()
+        AirportCode=tk.StringVar()
+        AirportName=tk.StringVar()
+        DepartureDate=tk.StringVar()
+        ContinentCode=tk.StringVar()
+        ContinentName=tk.StringVar()
+
+        headers=['PassengerId', 'FName','Lname', 'Gender ', 'Age', 'Nationality', 'Airport_Code', 'Departure_Date', 'Pilot_Name', 'Flight_status', 'Airport_name', 'Country_Code', 'Country_Name', 'Continent_Code', 'Continent_Name']
+        DataList = [PassengerId, FName, LName, Gender, Age, Nationality, CountryCode, CountryName, AirportCode, AirportName, DepartureDate, ContinentCode, ContinentName]
+        ColNameList = ['id', 'fname', 'lname', 'gender', 'age', 'nationality', 'country_code', 'country_name', 'airport_code', 'airport_name', 'departure_date', 'continent_code', 'continent_name']
+
+        def viewpassengersdb():
+            PassengerList.delete(1, tk.END)
+            for row in pbe.ViewAllPassengers():
+                PassengerList .insert(tk.END, row)
+        
+        def searchpassengersdb():
+            PassengerList.delete(1, tk.END)
+            SearchTerm = {}
+            for Name, Data in zip(ColNameList, DataList):
+                if Data.get()!='':
+                    SearchTerm[Name] = Data.get()
+            for row in pbe.SearchPassenger(SearchTerm):
+                 PassengerList.insert(tk.END, row)
+        
+        def passengersdatafill(event):
+            global sd
+            searchflight =  PassengerList.curselection()[0]
+            sd =  PassengerList.get(searchflight)
+            info_to_show = 'Data in Record:\n' + '\n'.join([a+": "+b for a, b in zip(headers, sd)])
+            messagebox.showinfo(title="Selected Record", message=info_to_show)
+
+
+        # Frames
+        main_frame = tk.Frame(self.root)
+        main_frame.pack(fill='both', expand=True)
+
+        title_frame = tk.Frame(main_frame)
+        title_frame.columnconfigure(0, weight=1)
+        title_frame.rowconfigure(0,weight=1)
+        title_frame.pack(fill='x')
+
+        data_frame = tk.Frame(main_frame, padx=5, pady=3)
+        data_frame.columnconfigure(0, weight=2)
+        data_frame.columnconfigure(1, weight=3)
+        data_frame.rowconfigure(0, weight=1)
+        data_frame.pack(fill='both', expand=True)
+        data_frame_left = tk.Frame(data_frame,bg='green')
+        data_frame_right = tk.Frame(data_frame,bg='red')
+        data_frame_right.rowconfigure(0, weight=1)
+        data_frame_right.columnconfigure(0, weight=1)
+        data_frame_left.grid(row=0, column=0, sticky='nwes')
+        data_frame_right.grid(row=0, column=1, sticky='nwes')
+
+        btn_frame = tk.Frame(main_frame)
+        btn_frame.pack(fill='x', expand=True, side='bottom')
+
+        # Labels and Entryboxes
+        self.lblTitle = tk.Label(title_frame, text="Flight Enquiry System", font='Arial 20 bold')
+        self.lblTitle.grid(row=0, column=0, sticky='nwes')
+
+        self.lblPassengerId = tk.Label(data_frame_left, text="Passenger ID: ", padx=5, pady=3)
+        self.lblPassengerId.grid(row=0, column=0, sticky='w')
+        self.txtPassengerId = tk.Entry(data_frame_left, textvariable=PassengerId, width=20)
+        self.txtPassengerId.grid(row=0, column=1)
+
+        self.lblFName = tk.Label(data_frame_left, text="First Name: ", padx=5, pady=3)
+        self.lblFName.grid(row=1, column=0, sticky='w')
+        self.txtFName = tk.Entry(data_frame_left, textvariable=FName, width=20)
+        self.txtFName.grid(row=1, column=1)
+
+        self.lblLName = tk.Label(data_frame_left, text="Last Name: ", padx=5, pady=3)
+        self.lblLName.grid(row=2, column=0, sticky='w')
+        self.txtLName = tk.Entry(data_frame_left, textvariable=LName, width=20)
+        self.txtLName.grid(row=2, column=1)
+
+        self.lblGender = tk.Label(data_frame_left, text="Gender: ", padx=5, pady=3)
+        self.lblGender.grid(row=3, column=0, sticky='w')
+        self.rdiobtnMale = tk.Radiobutton(data_frame_left, text="Male", variable=Gender, value='Male')
+        self.rdiobtnMale.grid(row=3, column=1)
+        self.rdiobtnFemale = tk.Radiobutton(data_frame_left, text='Female', variable=Gender, value='Female')
+        self.rdiobtnFemale.grid(row=3, column=2)
+
+        # self.txtGender = tk.Entry(data_frame_left, textvariable=Gender, width=20)
+        # self.txtGender.grid(row=3, column=1)
+
+        self.lblAge = tk.Label(data_frame_left, text="Age: ", padx=5, pady=3)
+        self.lblAge.grid(row=4, column=0, sticky='w')
+        self.txtAge = tk.Entry(data_frame_left, textvariable=Age, width=20)
+        self.txtAge.grid(row=4, column=1)
+
+        self.lblNationality = tk.Label(data_frame_left, text="Nationality: ", padx=5, pady=3)
+        self.lblNationality.grid(row=5, column=0, sticky='w')
+        self.txtNationality = tk.Entry(data_frame_left, textvariable=Nationality, width=20)
+        self.txtNationality.grid(row=5, column=1)
+
+        self.lblCountryCode = tk.Label(data_frame_left, text="Country Code: ", padx=5, pady=3)
+        self.lblCountryCode.grid(row=6, column=0, sticky='w')
+        self.txtCountryCode = tk.Entry(data_frame_left, textvariable=CountryCode, width=20)
+        self.txtCountryCode.grid(row=6, column=1)
+
+        self.lblCountryName = tk.Label(data_frame_left, text="Country Name: ", padx=5, pady=3)
+        self.lblCountryName.grid(row=7, column=0, sticky='w')
+        self.txtCountryName = tk.Entry(data_frame_left, textvariable=CountryName, width=20)
+        self.txtCountryName.grid(row=7, column=1)
+
+        self.lblAirportCode = tk.Label(data_frame_left, text="Airport Code: ", padx=5, pady=3)
+        self.lblAirportCode.grid(row=8, column=0, sticky='w')
+        self.txtAirportCode = tk.Entry(data_frame_left, textvariable=AirportCode, width=20)
+        self.txtAirportCode.grid(row=8, column=1)
+
+        self.lblAirportName = tk.Label(data_frame_left, text="Airport Name: ", padx=5, pady=3)
+        self.lblAirportName.grid(row=9, column=0, sticky='w')
+        self.txtAirportName = tk.Entry(data_frame_left, textvariable=AirportName, width=20)
+        self.txtAirportName.grid(row=9, column=1)
+
+        self.lblDepartureDate = tk.Label(data_frame_left, text="Departure Date: ", padx=5, pady=3)
+        self.lblDepartureDate.grid(row=10, column=0, sticky='w')
+        self.txtDepartureDate = tk.Entry(data_frame_left, textvariable=DepartureDate, width=20)
+        self.txtDepartureDate.grid(row=10, column=1)
+
+        self.lblContinentCode = tk.Label(data_frame_left, text="Continent Code: ", padx=5, pady=3)
+        self.lblContinentCode.grid(row=12, column=0, sticky='w')
+        self.txtContinentCode = tk.Entry(data_frame_left, textvariable=ContinentCode, width=20)
+        self.txtContinentCode.grid(row=12, column=1)
+
+        self.lblContinentName = tk.Label(data_frame_left, text="Continent Name: ", padx=5, pady=3)
+        self.lblContinentName.grid(row=13, column=0, sticky='w')
+        self.txtContinentName = tk.Entry(data_frame_left, textvariable=ContinentName, width=20)
+        self.txtContinentName.grid(row=13, column=1)
+
+
+       
+
+        sbVertical = tk.Scrollbar(data_frame_right, orient='vertical')
+        sbVertical.grid(row=0, column=1, sticky='ns')
+        sbHorizontal = tk.Scrollbar(data_frame_right, orient='horizontal')
+        sbHorizontal.grid(row=1, column=0, sticky='we')
+
+        PassengerList = tk.Listbox(data_frame_right, font='Arial 12 bold', yscrollcommand=sbVertical.set, xscrollcommand=sbHorizontal.set)
+        PassengerList.bind("<<ListboxSelect>>", passengersdatafill)
+        PassengerList.grid(row=0, column=0, sticky='nesw')
+        sbVertical.config(command= PassengerList.yview)
+        sbHorizontal.config(command= PassengerList.xview)
+        PassengerList.insert(tk.END, headers)
+
+        # Buttons
+        self.btnSearch = tk.Button(btn_frame, text="Search", command=searchpassengersdb)
+        self.btnSearch.pack(side='right', padx=5, pady=3)
+        self.btnDisplay = tk.Button(btn_frame, text='Display All', command=viewpassengersdb)
+        self.btnDisplay.pack(side='right', padx=5, pady=3)
 
 
 if __name__ == "__main__":
